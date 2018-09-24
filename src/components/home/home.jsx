e204de9af060f9cfe0113df5ec20d324ffa398fb
raphaeljq <raphaeljq@gmail.com>
2018-09-24 12:23:08 -0300

    tentativa mais de conseguir que o form funcione

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -128,7 +128,7 @@ const Home = () => (
       </div>
 
       <div class="col-12 col-md-6 ml-auto pt-5 pt-md-0">
-        <form method="post" name="think" netlify>
+        <form name="contato" method="POST" netlify>
           <div class="row">
             <div class="col">
               <input type="text" class="form-control" placeholder="First name"/>

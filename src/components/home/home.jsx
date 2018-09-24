8f3e40a761518bb226c0674c15fed7ee835336ef
raphaeljq <raphaeljq@gmail.com>
2018-09-24 12:16:45 -0300

    correcao novamente no form para testar o netlify

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -128,7 +128,7 @@ const Home = () => (
       </div>
 
       <div class="col-12 col-md-6 ml-auto pt-5 pt-md-0">
-        <form name="think" netlify>
+        <form method="post" name="think" netlify>
           <div class="row">
             <div class="col">
               <input type="text" class="form-control" placeholder="First name"/>

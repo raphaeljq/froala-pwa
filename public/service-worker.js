f684d482843a9e67e1e95de3f15ddd72ab1a336a
raphaeljq <raphaeljq@gmail.com>
2018-09-24 17:12:05 -0300

    foram adicionadas correcoes no formulario original e no form oculto alem de ser adicionado serviceworker pela primeira vez

---
+++ public/service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'pwa-cache-v1';
+
+const urlsToCache = [
+  "/",
+  "https://cdnjs.cloudflare.com/ajax/libs/froala-design-blocks/1.0.2/css/froala_blocks.min.css",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",
+  "https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css",
+
+  "https://code.jquery.com/jquery-3.2.1.slim.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js",
+   "https://unsplash.it/600.jpg?image=251",
+   "https://unsplash.it/600.jpg?image=252",
+   "https://unsplash.it/600.jpg?image=253",
+   "https://unsplash.it/1200/768.jpg?image=254",
+    "https://unsplash.it/600.jpg?image=255",
+    "https://unsplash.it/600.jpg?image=256",
+  "static/css/main.de320ce3.css",
+  "static/css/main.de320ce3.css.map",
+  "static/js/main.959402d6.js",
+  "static/js/main.959402d6.js.map",
+  "static/media/avatar1.38f11781.png",
+  "static/media/avatar2.2b64330d.png",
+  "static/media/avatar3.2dde3e37.png",
+  "static/media/avatar4.2f7e5237.png",
+  "static/media/avatar5.f1f5a4df.png",
+  "static/media/avatar6.815f22fc.png",
+  "static/media/avatar7.da993183.png",
+  "static/media/avatar8.54dd84c6.png",
+  "static/media/avatar9.3e06f131.png",
+  "static/media/cafe-3537801_1920.688edae9.jpg",
+  "static/media/city-569093_1920.8575527b.jpg",
+  "static/media/manhattan-336708_1920.5f9a4fa8.jpg",
+  "static/media/tree-736885_1280.d1ba9782.jpg"
+]
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function (cache) {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+  self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        }))
+      )
+    );
+  });
+
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -103,9 +103,7 @@ const Home = () => (
 
 </section> 
 <section class="fdb-block pt-0 fp-active" data-block-type="contacts" data-id="1" draggable="true">
-  <div class="container-fluid p-0 pb-md-5">
-    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.8444388087937!2d26.101253041406952!3d44.43635311654287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1ff4770adb5b7%3A0x58147f39579fe6fa!2zR3J1cHVsIFN0YXR1YXIgIkPEg3J1yJthIEN1IFBhaWHIm2Ui!5e0!3m2!1sen!2sro!4v1507381157656" width="100%" height="300" frameborder="0" allowfullscreen=""></iframe>
-  </div>
+  
   <div class="container">
     <div class="row mt-5">
       <div class="col-12 col-md-6 col-lg-5">
@@ -133,22 +131,22 @@ const Home = () => (
 
           <div class="row">
             <div class="col">
-              <input type="text" class="form-control" placeholder="First name"/>
+              <input type="text" name="first_name" class="form-control" placeholder="First name"/>
             </div>
             <div class="col">
-              <input type="text" class="form-control" placeholder="Last name"/>
-            </div>
-          </div>
-
-          <div class="row mt-4">
-            <div class="col">
-              <input type="email" class="form-control" placeholder="Enter email"/>
+              <input type="text" name="last_name" class="form-control" placeholder="Last name"/>
             </div>
           </div>
 
           <div class="row mt-4">
             <div class="col">
-              <input type="email" class="form-control" placeholder="Subject"/>
+              <input type="email" name="email" class="form-control" placeholder="Enter email"/>
+            </div>
+          </div>
+
+          <div class="row mt-4">
+            <div class="col">
+              <input type="email" name="assunto" class="form-control" placeholder="Subject"/>
             </div>
           </div>
           <div class="row mt-4">

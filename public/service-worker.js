07eea22e1454a9900e7bb82951c3163c12f7aa36
raphaeljq <raphaeljq@gmail.com>
2018-09-24 18:28:07 -0300

    Foi alterado o tamanho do arquivo png para 192x192 pois aparentemente sem ele nao aparece o icone png

--- public/service-worker.js
+++ public/service-worker.js
@@ -37,32 +37,39 @@ const urlsToCache = [
   "static/media/tree-736885_1280.d1ba9782.jpg"
 ]
 
+// A primeira vez que o usuário inicia a PWA, 'install' é acionado.
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function (cache) {
-          return cache.addAll(urlsToCache);
-        })
-    );
-  });
-
-  self.addEventListener("activate", event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-      caches.keys().then(keyList =>
-        Promise.all(keyList.map(key => {
-          if (!cacheWhitelist.includes(key)) {
-            return caches.delete(key);
-          }
-        }))
-      )
-    );
-  });
-
-  self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // Abre um cache e armazena nossos arquivos em cache
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
+  );
+});
+// Elimina caches antigos que não sejam os atuais.
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
+
+// Quando a página da Web vai buscar arquivos, nós interceptamos esse pedido e servimos os arquivos correspondentes
+// se tivemos
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (event.request.cache === 'only-if-cache') {
+        event.request.mode = 'same-origin'
+      }
+      return response || fetch(event.request);
+    })
+  );
+});
